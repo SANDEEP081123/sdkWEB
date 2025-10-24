bd217cbe2a3acb5b7e9d69e2b31ee47c4073fec5
SANDEEP081123 <sandeepnelapogula68@gmail.com>
2025-10-24 17:40:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let sectionHome = document.getElementById("sectionhome");
+
+document.getElementById("clickme1").addEventListener("click", onClickMe1);
+
+
+function onClickMe1() {
+    sectionHome.style.backgroundColor = "#11ce11";
+};
